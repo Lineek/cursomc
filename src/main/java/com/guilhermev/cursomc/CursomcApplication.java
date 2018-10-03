63e7d96adada9a51f51d8822704e338bf044abc8
Guilherme Vicchietti Iannotta <giannotta@stefanini.com>
2018-10-03 10:25:16 -0300

    Estado e Cidade

--- src/main/java/com/guilhermev/cursomc/CursomcApplication.java
+++ src/main/java/com/guilhermev/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.guilhermev.cursomc;
 
 import com.guilhermev.cursomc.domain.Categoria;
+import com.guilhermev.cursomc.domain.Cidade;
+import com.guilhermev.cursomc.domain.Estado;
 import com.guilhermev.cursomc.domain.Produto;
 import com.guilhermev.cursomc.repositories.CategoriaRepository;
+import com.guilhermev.cursomc.repositories.CidadeRepository;
+import com.guilhermev.cursomc.repositories.EstadoRepository;
 import com.guilhermev.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,11 +20,15 @@ public class CursomcApplication implements CommandLineRunner {
 
     private final CategoriaRepository categoriaRepository;
     private final ProdutoRepository produtoRepository;
+    private final EstadoRepository estadoRepository;
+    private final CidadeRepository cidadeRepository;
 
     @Autowired
-    public CursomcApplication(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository) {
+    public CursomcApplication(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository, EstadoRepository estadoRepository, CidadeRepository cidadeRepository) {
         this.categoriaRepository = categoriaRepository;
         this.produtoRepository = produtoRepository;
+        this.estadoRepository = estadoRepository;
+        this.cidadeRepository = cidadeRepository;
     }
 
     public static void main(String[] args) {
@@ -45,5 +53,19 @@ public class CursomcApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+//        est1.getCidades().addAll(Arrays.asList(c1));
+//        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 }
